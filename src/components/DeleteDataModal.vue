35972ec748e83508222c919d6287bdde642697dd
diamondto <enzoblackred@gmail.com>
2024-07-26 19:31:25 +0700

    [feat/activity] successfull make modal delete

---
+++ src/components/DeleteDataModal.vue
@@ -0,0 +1,98 @@
+<template>
+    <div id="DeleteComponent">
+        <div 
+            v-if="props?.loading || props?.isOpenModal" 
+            id="modal-bg" class="w-full h-full z-20 absolute fixed top-0 blur-background overflow-hidden">
+        </div>
+        <!-- loading -->
+        <div
+            v-if="props?.loading" 
+            class="sm:w-[385px] sm:min-w-[30vw] min-w-[60vw] min-h-[30vh] flex flex-col items-center gap-2 -translate-y-1/2 p-6 top-1/2 left-1/2 -translate-x-1/2 absolute z-20">
+            <img src="../../assets/icon/spinner.svg" alt="spinner">
+        </div>
+        <transition v-if="props?.isOpenModal && !props?.loading"  name="fade">
+            <div    
+                id="modal-box" class="w-[385px] min-w-[30vw]  lg:w-[385px] lg:min-w-[30vw] lg:min-h-[20vh] flex flex-col items-center gap-2 -translate-y-1/2 p-6 bg-white rounded-lg top-1/2 left-1/2 -translate-x-1/2 shadow-md absolute z-20 border border-solid border-gray-200 fixed">
+                <WarningIcon></WarningIcon>
+                <h2 class="text-xl text-blue-primary font-medium mt-9">{{props?.responseModal?.title}} </h2>
+                <p class="text-center mt-4">{{props?.responseModal?.message}}</p>
+                <template v-if="!props.isConfirmModal">
+                    <div class="w-full flex justify-center">
+                        <button id="modal-close" class="py-2 px-6 bg-sky-400 rounded-lg text-white mb-7"
+                            @click="onToggle(false)"
+                        > {{props.confirmButton}} </button>
+                    </div>
+                </template>
+                <template v-if="props.isConfirmModal">
+                    <div class="w-full flex justify-center">
+                        <button id="modal-close" class="py-2 px-6 bg-sky-400 rounded-lg text-white mb-7"
+                            @click="onToggle(true)"
+                        >{{props.confirmButton}} </button>
+                        <button id="modal-close" class="py-2 px-6 bg-red-primary rounded-lg text-white mb-7 ml-2"
+                            @click="onToggle(false)"
+                        >{{props.denyButton}} </button>
+                    </div>
+                </template>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive, watch, computed, onMounted, onBeforeMount, defineProps, defineEmits, } from 'vue';
+import WarningIcon from '@/assets/svg/WarningIcon.vue';
+
+const props = defineProps({
+    loading:{
+        default: false
+    },
+    responseModal:{
+        type: Object,
+        default() {
+            return {
+                title: 'Something Wrong',
+                message: 'Please contact your admin',
+            }
+        }
+    },
+    confirmButton: {
+        default: 'Ok'
+    },  
+    denyButton: {
+        default: 'No'
+    },
+    // for add name modal unique
+    nameModal: {
+        default: ''
+    },
+    isConfirmModal: {
+        default: false
+    },
+    isOpenModal: {
+        default: false
+    }
+})
+
+const emit = defineEmits([
+            'confirm',
+            'confirmDelete',
+            'isOpenModelCloseGeneral',
+        ]);
+
+        function onToggle(data) {
+            props.isOpenModal = !props.isOpenModal;
+            const payload = {
+                name: props.nameModal,
+                value: data
+            }
+
+            emit('isOpenModelCloseGeneral', payload)
+        }
+
+</script>
+
+<style scoped>
+.blur-background{
+    background-color: rgba(255,255,255, 0.8);
+}
+</style>
--- src/views/activity/Activity.vue
+++ src/views/activity/Activity.vue
@@ -1,19 +1,19 @@
 <template>
-    <div id="ActivityComponent" class="flex justify-center min-h-screen mt-28 lg:mt-28 pt-2 lg:pt-10">
+    <div 
+        id="ActivityComponent" class="flex justify-center min-h-screen ">
         <div class="relative overflow-y-auto overflow-x-hidden z-1 container">
-            <div class="flex justify-between px-4">
+            <div class="flex flex-row fixed justify-between px-4 container bg-gray-50 dark:bg-gray-900 mt-24 lg:mt-24 py-3 lg:pt-10 ">
                 <div>
                     <h1 class="lg:text-3xl md:text-3xl text-xl sm: font-bold">ACTIVITY</h1>
                 </div>
-                <div class="flex justify-center align-center md:px-6 lg:px-6 px-4 py-2 rounded-full bg-sky-400 text-sm text-white">
+                <div class="flex justify-center align-center md:px-6 lg:px-6 px-4 py-2 rounded-full bg-sky-400 hover:bg-sky-600 text-sm text-white cursor-pointer">
                     <span class="material-icons-outlined md:text-2xl lg:text-2xl text-lg">
                         add
                     </span>
                     <h1 class="md:text-2xl lg:text-2xl text-lg ">&nbsp;Tambah</h1>
                 </div>
             </div>
-            <div class="flex flex-row flex-wrap justify-center">
-
+            <div class="flex flex-row flex-wrap justify-center mt-24 lg:mt-28 pt-20 lg:pt-24">
                 <div 
                     v-for="(item, index) in activity_dummy_data.data"
                     :key="index"
@@ -21,15 +21,20 @@
 
                 >
                     <div class="border border-solid border-gray-200 shadow-lg rounded-md px-4 py-6">
-                        <div class="h-[12rem] text-2xl font-semibold">
+                        <div 
+                            @click="onDetail(item?.id)"
+                            class="h-[12rem] text-2xl font-semibold cursor-pointer">
                             {{ item?.title }}
                         </div>
                         <div class="flex justify-between align-center text-gray-500">
-                            <div class="text-xl">
+                            <div class="flex items-center align-center text-xl">
                                 {{ convertDate(item?.created_at) }}
                             </div>
-                            <div>
-                                <span class="material-icons-outlined text-gray-500 cursor-pointer text-3xl" style="line-height: 1rem;">
+                            <div class="flex items-center align-center">
+                                <span 
+                                    class="material-icons-outlined text-gray-500 cursor-pointer text-3xl" style="line-height: 1rem;" 
+                                    @click="onDelete(item?.created_at)"
+                                >
                                     delete
                                 </span>
                             </div>
@@ -37,20 +42,64 @@
                     </div>
                 </div>
             </div>
-            
         </div>
     </div>
+    <DeleteDataModal
+    :loading="loading" 
+        :isOpenModal="isOpenModalGlobal"
+        :confirmButton="nameModalButtonGlobal" 
+        :nameModal="nameModal" 
+        :isConfirmModal="isConfirmModalGlobal"  
+        @isOpenModelCloseGeneral="isOpenModelCloseServer"
+        :responseModal="responseModalGlobal" 
+    >
+
+    </DeleteDataModal>
 </template>
 
 <script setup>
 import { ref, reactive, watch, computed, onMounted, onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
 import { activity_dummy_data } from '@/utilize/DataDummy';
+import DeleteDataModal from '@/components/DeleteDataModal.vue';
 import { utilize } from '@/utilize/index'
+
+const router = useRouter();
+
+const isOpenModalGlobal = ref(false);
+const nameModalButtonGlobal = ref('Yes');
+const nameModal = ref('delete_activity');
+const isConfirmModal = ref(false);
+const responseModalGlobal = ref(null);
+const isConfirmModalGlobal = ref(false);
 
 
 function convertDate(data){
     return utilize.convertTimeDate(data);
 }
+
+function isOpenModelCloseServer (event) {
+    isConfirmModal.value = false;
+    isOpenModalGlobal.value = false;
+    responseModalGlobal.value = null;
+    isConfirmModalGlobal.value = false;
+}
+
+const onDelete = () => {
+    isConfirmModal.value = true;
+    isOpenModalGlobal.value = true;
+    isConfirmModalGlobal.value = true;
+    responseModalGlobal.value = {
+        title: 'Delete Data',
+        message: 'Are you sure want delete this data ?'
+    }
+}
+
+const onDetail = (data) =>{
+    router.push(`activity/detail/${data}`)
+}
+
+
 </script>
 
 <style scoped>
