577042b93d147e48eba817a02c4ded3551a37621
diamondto <enzoblackred@gmail.com>
2024-07-30 23:42:34 +0700

    [develop] add photo profile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,7 +17,35 @@
                         <h3 class="lg:text-4xl md:text-4xl text-xl text-white font-bold py-5">App Todo List</h3>
                     </div>
                     <div class="w-1/2 flex justify-end items-center">
-                        <div class="flex justify-center align-center md:px-6 lg:px-6 px-4 py-2 rounded-full bg-white text-sm text-white cursor-pointer lg:mr-6" 
+                        <div class="flex item-center mr-3">
+                            <div class="w-12 h-12 rounded-full relative">
+                                <button class="cursor-pointer"
+                                    @click="isOptionsExpanded = !isOptionsExpanded"
+                                    @blur="isOptionsExpanded = false"
+                                >
+                                    <img class="w-12 h-12" src="../assets/image/user_1.png" alt="user">
+                                </button>
+                                <transition
+                                    enter-active-class="transform transition duration-500 ease-custom"
+                                    enter-class="-translate-y-1/2 scale-y-0 opacity-0"
+                                    enter-to-class="translate-y-0 scale-y-100 opacity-100"
+                                    leave-active-class="transform transition duration-300 ease-custom"
+                                    leave-class="translate-y-0 scale-y-100 opacity-100"
+                                    leave-to-class="-translate-y-1/2 scale-y-0 opacity-0"
+                                >
+                                    <ul
+                                        v-show="isOptionsExpanded"
+                                        class="absolute left-0 right-0 mb-4 bg-white divide-y rounded-lg shadow-lg overflow-hidden w-48"
+                                    >    
+                                        <li class="px-3 py-2 transition-colors duration-300 hover:bg-gray-200">
+                                            Anjasmara Dwi Setiadi
+                                        </li>
+                                        
+                                    </ul>
+                                </transition>
+                            </div>
+                        </div>
+                        <div class="flex justify-center align-center md:px-6 lg:px-6 px-4 py-2 rounded-full bg-white text-sm text-white cursor-pointer lg:mr-6 mr-2 " 
                             @click="logout()"
                         >
                             <h3 class="lg:text-xl md:text-2xl text-xl text-sky-400 font-bold">
@@ -41,6 +69,7 @@ import { errorHandle } from '@/utilize/HandleError';
 import Swal from 'sweetalert2';
 const authStore = useAuthStore()
 const router = useRouter();
+const isOptionsExpanded = ref(false);
 
 const nameRoutePath = ref('');
 
